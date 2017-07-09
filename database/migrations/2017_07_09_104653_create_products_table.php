458508489bdc99f884d32b47c4146e3f1520c68a
caoxiang <caoxiang@yeah.net>
2017-07-09 21:47:41 +0800

    redesign databases and auth structure

--- database/migrations/2017_07_09_104653_create_products_table.php
+++ database/migrations/2017_07_09_104653_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAreasTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateAreasTable extends Migration
      */
     public function up()
     {
-        Schema::create('area', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('city_id');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateAreasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('area');
+        Schema::dropIfExists('products');
     }
 }
--- database/migrations/2017_07_09_104755_create_areas_table.php
+++ database/migrations/2017_07_09_104755_create_areas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCityTable extends Migration
+class CreateAreasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateCityTable extends Migration
      */
     public function up()
     {
-        Schema::create('city', function (Blueprint $table) {
+        Schema::create('areas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 200)->comment('名字');
-            $table->tinyInteger('status')->comment('状态');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateCityTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('city');
+        Schema::dropIfExists('areas');
     }
 }
