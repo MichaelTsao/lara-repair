56b93a9353388bc5d0eb0cdda3eee5bd0f796f4d
caoxiang <caoxiang@yeah.net>
2017-07-09 21:48:09 +0800

    add phpstorm ide helper

--- database/migrations/2017_07_09_104805_create_countries_table.php
+++ database/migrations/2017_07_09_104805_create_countries_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompanyTable extends Migration
+class CreateCountriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateCompanyTable extends Migration
      */
     public function up()
     {
-        Schema::create('company', function (Blueprint $table) {
+        Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 200)->comment('名字');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateCompanyTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('company');
+        Schema::dropIfExists('countries');
     }
 }
